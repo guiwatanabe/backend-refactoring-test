4baa93eb11c053d2b1ef8b7f894da9f60a0c27db
Guilherme Watanabe <guiwatanabe@hotmail.com>
2025-08-21 16:38:09 -0300

    Corrige fluxo oauth2 Swagger

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -176,20 +176,21 @@ return [
                 ],
                 */
 
-                'passport' => [ // Unique name of security
+                'bearerAuth' => [ // Unique name of security
                     'type' => 'oauth2', // The type of the security scheme. Valid values are "basic", "apiKey" or "oauth2".
                     'description' => 'Laravel passport oauth2 security.',
                     'in' => 'header',
                     'scheme' => 'https',
                     'flows' => [
-                        "password" => [
-                            "authorizationUrl" => config('app.url') . '/oauth/authorize',
-                            "tokenUrl" => config('app.url') . '/oauth/token',
-                            "refreshUrl" => config('app.url') . '/token/refresh',
-                            "scopes" => []
+                        'password' => [
+                            'authorizationUrl' => config('app.url').'/oauth/authorize',
+                            'tokenUrl' => config('app.url').'/oauth/token',
+                            'refreshUrl' => config('app.url').'/token/refresh',
+                            'scopes' => [],
                         ],
                     ],
                 ],
+
                 /*
                 'sanctum' => [ // Unique name of security
                     'type' => 'apiKey', // Valid values are "basic", "apiKey" or "oauth2".
@@ -204,14 +205,13 @@ return [
                  * Examples of Securities
                 */
                 [
+                    'bearerAuth' => [],
+
                     /*
                     'oauth2_security_example' => [
                         'read',
                         'write'
-                    ],
-
-                    'passport' => []
-                    */
+                    ],*/
                 ],
             ],
         ],
@@ -298,4 +298,3 @@ return [
         ],
     ],
 ];
-
